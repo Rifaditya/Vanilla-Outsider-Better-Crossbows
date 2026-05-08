f98bfd9d6ec0d23c7f888803291009463091c0c3
Rifa <fm.rifaditya@gmail.com>
2026-05-08 09:56:40 +0700

    release: 1.0.0+build.4 - Adjusted Mixin priority for better compatibility

--- src/main/java/net/vanillaoutsider/bettercrossbows/mixin/AnvilMenuMixin.java
+++ src/main/java/net/vanillaoutsider/bettercrossbows/mixin/AnvilMenuMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AnvilMenu.class)
+@Mixin(value = AnvilMenu.class, priority = 500)
 public class AnvilMenuMixin {
 
     @org.spongepowered.asm.mixin.Shadow
